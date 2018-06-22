697342b248d553434ae540cca9ebbf962c8d4211
Rajesh Verma <RAJESH_BHILAWAN@REDIFFMAIL.COM>
2018-06-22 19:50:32 +0530

    Add files via upload

---
+++ SureTrader.FrontOfficeTests/Controllers/EmployeeeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SureTrader.FrontOffice.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SureTrader.FrontOffice.Controllers.Tests
+{
+    [TestClass()]
+    public class EmployeeeControllerTests
+    {
+        [TestMethod()]
+        public void GetInfoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
