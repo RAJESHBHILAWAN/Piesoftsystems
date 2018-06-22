bd4171f3c63e4b62049e1ffb759cbd8943d98c67
Rajesh Verma <RAJESH_BHILAWAN@REDIFFMAIL.COM>
2018-06-22 19:51:49 +0530

    Add files via upload

---
+++ SureTrader.Services/_EmployeeRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SureTrader.Core.Contracts;
+using SureTrader.Core.CustomModel;
+using SureTrader.Core;
+
+namespace SureTrader.Services
+{
+   public class EmployeeeRepo : IEmployeee
+    {
+
+        public List<Employeee> GetAllUser()
+        {
+            using (var context = new myconcontext())
+            {
+
+                return context.Employeees.ToList();
+
+            }
+
+        }
+
+    }
+}
